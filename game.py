1a8cda35904df40b953f058288874748a57de8a5
Karambami <114066691+Karambami@users.noreply.github.com>
2022-10-05 12:08:46 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,18 +1,4 @@
-human_turn = 'Paper'
-computer_turn = 'Rock'
-
-if human_turn == computer_turn:
-    print('Draw!')
-elif human_turn == 'Rock' and computer_turn == 'Scissors':
-    print('Human wins!')
-elif human_turn == 'Paper' and computer_turn == 'Rock':
-    print('Human wins!')
-elif human_turn == 'Scissors' and computer_turn == 'Paper':
-    print('Human wins!')
-else:
-    print('Computer wins!')
-
-    import random
+import random
 turns = ['rock', 'paper', 'scissors']
 human_turns = []
 computer_turns = []
